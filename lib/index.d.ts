11c78455aaf2a9ee56cb280563fabbddcb2345a9
1amageek <nori@stamp.team>
2024-06-04 19:58:39 +0900

    feat: Add computeExecutablePath and detectPlatform functionsThe new functions computeExecutablePath and detectPlatform have been added to the codebase

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -8,6 +8,21 @@ interface MuppeteerOptions {
     hosts?: string[];
     folderName?: string;
 }
+interface ComputeExecutablePathOptions {
+    platform: BrowserPlatform;
+    buildId: string;
+    cacheDir: string;
+}
+export declare const computeExecutablePath: (options: ComputeExecutablePathOptions) => string;
+export declare const detectPlatform: () => BrowserPlatform;
+export declare const defaultHosts: string[];
+interface Stats {
+    executablePath: string;
+    revision: string;
+    platform: BrowserPlatform;
+}
+export declare const getStats: (cacheDir: string) => Stats | null;
+export declare const saveStats: (cacheDir: string, stats: Stats) => void;
 export declare const getSnapshotsDir: (options: MuppeteerOptions) => string;
 declare const muppeteer: (options?: MuppeteerOptions) => Promise<string>;
 export default muppeteer;
